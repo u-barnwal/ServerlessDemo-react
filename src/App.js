472239641fc56b23b5f2b029edb09922120cf69a
Utsav Barnwal <u.barnwal36@gmail.com>
2021-06-16 02:57:14 +0530

    use axios to load and show data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const fetchData = async () => {
+    const results = await axios.get("./.netlify/functions/helloWorld");
+    setMessage(results.data.message);
+  };
+
+  useEffect(() => fetchData(), []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message}</p>
       </header>
     </div>
   );
